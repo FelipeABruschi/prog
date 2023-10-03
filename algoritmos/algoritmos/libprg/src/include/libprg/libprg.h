f079788439a25ce1bb5e8b8af125e5ccf3093e12
FelipeABruschi <110953697+FelipeABruschi@users.noreply.github.com>
2023-10-03 15:21:09 -0300

    algoritmo.2

---
+++ algoritmos/algoritmos/libprg/src/include/libprg/libprg.h
@@ -0,0 +1,52 @@
+#ifndef LIBPRG_LIBPRG_H
+#define LIBPRG_LIBPRG_H
+
+typedef struct {
+    double value;
+    int error;
+} result_t;
+
+typedef struct
+{
+    int *valores;
+    int tam_max;
+    int n_elementos;
+}Lista;
+
+typedef struct
+{
+    int *valores;
+    int inicio;
+    int fim;
+    int capacidade;
+}Fila;
+
+typedef struct
+{
+    int *valores;
+    int topo;
+    int capacidade;
+}Pilha;
+
+void cria_lista(Lista *lista, int tam_max);
+void imprime_lista(Lista *lista);
+int busca_linear(Lista *lista, int x);
+void inserir_n(Lista* lista, int x);
+void remove_n(Lista *lista, int x);
+
+void cria_fila(Fila *fila, int capacidade);
+void imprime_fila(Fila *fila);
+
+void cria_pilha(Pilha *pilha, int capacidade);
+void imprime_pilha(Pilha *pilha);
+void push(Pilha *pilha, int x);
+int pop(Pilha *pilha);
+int size(Pilha *pilha);
+
+typedef enum { SUM, SUB } operation_t;
+
+result_t sub(double a, double b);
+result_t sum(double a, double b);
+result_t compute(double a, double b, operation_t op);
+
+#endif
---
+++ algoritmos/algoritmos/main.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include "libprg/libprg.h"
+#include <stdlib.h>
+
+int main()
+{
+    /*Lista minha_lista;
+    cria_lista(&minha_lista, 1000);
+
+    minha_lista.valores[0] = 10;
+    minha_lista.valores[1] = 20;
+    minha_lista.valores[2] = 25;
+    minha_lista.n_elementos = 3;
+
+    imprime_lista(&minha_lista);
+
+    printf("%d\n", busca_linear(&minha_lista, 10));
+
+    inserir_n(&minha_lista, 12);
+
+    imprime_lista(&minha_lista);
+
+    Fila minha_fila;
+    cria_fila(&minha_fila, 10);
+
+    imprime_fila(&minha_fila);*/
+
+    Pilha minhapilha;
+    cria_pilha(&minhapilha, 12);
+
+    push(&minhapilha, 10);
+    push(&minhapilha, 5);
+    push(&minhapilha, 2);
+
+    imprime_pilha(&minhapilha);
+
+    printf("%d\n", size(&minhapilha));
+
+    return 0;
+}
